2c3b1456c0e0f15e3eac2c26614e2d6cbd23be07
Akseleron <165706442+Akseleron@users.noreply.github.com>
2025-11-08 19:22:32 +0300

    Create ChunkRewriter class with constructor
    Added a placeholder class for chunk rewriting with a constructor.

--- src/main/java/dev/aks/worldrandomizer/chunk/ChunkRewriter.java
+++ src/main/java/dev/aks/worldrandomizer/chunk/ChunkRewriter.java
@@ -1,1 +1,17 @@
+package dev.aks.worldrandomizer.chunk;
 
+import dev.aks.worldrandomizer.MappingService;
+
+/**
+ * Заглушка. Когда решим реально переписывать чанки:
+ * - В конструкторе подпишемся на ServerChunkEvents.CHUNK_LOAD
+ * - В ServerTickEvents.END_SERVER_TICK будем выносить работу по частям (тик-бюджет)
+ * - Каждую позицию заменяем на mapping.mapBlockId(...)
+ */
+public final class ChunkRewriter {
+    private final MappingService mapping;
+
+    public ChunkRewriter(MappingService mapping) {
+        this.mapping = mapping;
+    }
+}
